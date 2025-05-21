95afe0d7e1de324c64ef9d692a452959109b1800
javierrcyb <jsr38819@email.vcss.edu>
2025-05-21 12:10:59 -0500

    Create Books

---
+++ Javascript/Project_Library/script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, editorial) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.editorial = editorial;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, editorial) {
+    const book = new Book(title, author, pages, editorial);
+    myLibrary.push(book);
+}
