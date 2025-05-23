79c4a3274b01817f4d0a63939d42b74a2258c136
javierrcyb <jsr38819@email.vcss.edu>
2025-05-22 23:04:12 -0500

    add book form

--- Javascript/Project_Library/script.js
+++ Javascript/Project_Library/script.js
@@ -1,6 +1,37 @@
 const myLibrary = [];
 
-function Book(title, author, pages, editorial) {
+const addBookButton = document.querySelector(".add-book");
+const bookForm = document.querySelector('dialog');
+const XbuttonClose = document.querySelector('.close');
+const formSubmission = document.querySelector('form');
+
+addBookButton.addEventListener('click', () => {
+  bookForm.showModal();
+});
+
+XbuttonClose.addEventListener('click', () => {
+    bookForm.close();
+});
+
+formSubmission.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  addBookToLibrary(
+    event.target.title.value,
+    event.target.author.value,
+    event.target.pages.value,
+    event.target.read.value
+  );
+
+  formSubmission.reset();
+  console.log(myLibrary);   
+});
+
+
+
+
+
+function Book(title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
@@ -8,12 +39,11 @@ function Book(title, author, pages, editorial) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.editorial = editorial;
-    this.read = false;
+    this.read = read;
     this.id = crypto.randomUUID();
 }
 
-function addBookToLibrary(title, author, pages, editorial) {
-    const book = new Book(title, author, pages, editorial);
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(title, author, pages, read);
     myLibrary.push(book);
 }
